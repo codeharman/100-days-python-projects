c09f3bb70a56169f47c199eb750a352ccd3f984d
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-04-24 23:32:48 +0530

    hangman easy version

--- day7/main.py
+++ day7/main.py
@@ -2,21 +2,25 @@
 
 import random
 
-class Game:
-  def __init__(self):
-    pass
+lists = ['hello', 'bye', 'why']
+word = random.choice(lists)
 
-  random_word = ['hello', 'bye', 'why']
-  random_num = random.choice(random_word)
-  lives_left = 5
-  value = input(f'guess the word: ')
+lives = 5
+placeholder = ['_'] * len(word)
 
-  def print(self):
-    for v in self.random_num:
-      if self.value == v:
-        print('true')
-      else:
-        print('false')
+while lives > 0 and '_' in placeholder:
+  print("Words: ", ' '.join(placeholder))
+  guess = input('guess the word: ').lower()
+  
+  if guess in word:
+    for i in range(len(word)):
+      if word[i] == guess:
+        placeholder[i] = guess
+  else:
+    lives -= 1
+    print('Wrong Answer, Lives left: ', lives)
 
-gg = Game()
-gg.print()
+if '_' not in placeholder:
+  print("Congrats you win")
+else:
+  print('better luck next time')
